c14edb94be0d85dd552cb1d73b2c28a4456c1134
DiegoChenTPG <2918388471601@ingenieria.usac.edu.gt>
2025-09-24 20:53:15 -0600

    fix: update procedimiento 1

--- Proyecto 1/scripts_mysql/procedimientos.sql
+++ Proyecto 1/scripts_mysql/procedimientos.sql
@@ -4,8 +4,25 @@ DROP PROCEDURE IF EXISTS show_info_movie_show;
 -- PRIMER PROCEDIMIENTO 
 CREATE PROCEDURE show_info_movie_show(IN p_title VARCHAR(1024))
 BEGIN
-  /* ===================== 1) Ficha básica ===================== */
-  SELECT 
+  WITH candidates AS (
+    SELECT tb.tconst
+    FROM title_basics tb 
+    WHERE tb.primaryTitle = p_title
+
+    UNION
+    SELECT tb.tconst
+    FROM title_basics tb
+    WHERE tb.originalTitle = p_title
+
+    UNION
+    SELECT a.titleId AS tconst
+    FROM akas a          
+    WHERE a.title = p_title
+  ),
+  uniq AS (
+    SELECT DISTINCT tconst FROM candidates
+  )
+  SELECT
     tb.tconst,
     tb.titleType,
     tb.primaryTitle,
@@ -14,115 +31,22 @@ BEGIN
     tb.startYear,
     tb.endYear,
     tb.runtimeMinutes,
-    (SELECT GROUP_CONCAT(DISTINCT bg.genre ORDER BY bg.genre SEPARATOR ', ')
-      FROM basics_genres bg WHERE bg.tconst = tb.tconst) AS genres,
+    GROUP_CONCAT(DISTINCT bg.genre ORDER BY bg.genre SEPARATOR ', ') AS genres,
     r.averageRating,
     r.numVotes,
-    EXISTS (SELECT 1 FROM episodes e WHERE e.parentTconst = tb.tconst LIMIT 1) AS isSeries,
-    EXISTS (SELECT 1 FROM episodes e WHERE e.tconst       = tb.tconst LIMIT 1) AS isEpisode
-  FROM title_basics tb
-  LEFT JOIN ratings r ON r.tconst = tb.tconst
-  WHERE tb.primaryTitle  = p_title
-    OR tb.originalTitle = p_title
-    OR EXISTS (SELECT 1 FROM akas a 
-                WHERE a.titleId = tb.tconst AND a.title = p_title)
+  
+    MAX(epar.parentTconst IS NOT NULL) AS isSeries,
+    MAX(eepi.tconst      IS NOT NULL) AS isEpisode
+  FROM uniq u
+  JOIN title_basics tb  ON tb.tconst = u.tconst
+  LEFT JOIN ratings r   ON r.tconst  = tb.tconst
+  LEFT JOIN basics_genres bg ON bg.tconst = tb.tconst
+  LEFT JOIN episodes epar ON epar.parentTconst = tb.tconst
+  LEFT JOIN episodes eepi ON eepi.tconst = tb.tconst
+  GROUP BY
+    tb.tconst, tb.titleType, tb.primaryTitle, tb.originalTitle,
+    tb.isAdult, tb.startYear, tb.endYear, tb.runtimeMinutes, r.averageRating, r.numVotes
   ORDER BY tb.startYear, tb.primaryTitle;
-
-  /* ===================== 2) AKAs + types/attributes ===================== */
-  SELECT
-    a.titleId AS tconst,
-    a.ordering,
-    a.title,
-    a.region,
-    a.isOriginalTitle,
-    (SELECT GROUP_CONCAT(DISTINCT atp.type ORDER BY atp.type SEPARATOR ', ')
-      FROM aka_types atp 
-      WHERE atp.titleId = a.titleId AND atp.ordering = a.ordering) AS types,
-  (SELECT GROUP_CONCAT(DISTINCT att.attribute ORDER BY att.attribute SEPARATOR ', ')
-      FROM aka_attributes att
-      WHERE att.titleId = a.titleId AND att.ordering = a.ordering) AS attributes
-  FROM akas a
-  WHERE EXISTS (
-    SELECT 1 FROM title_basics tb
-    WHERE tb.tconst = a.titleId
-      AND (
-        tb.primaryTitle  = p_title OR
-        tb.originalTitle = p_title OR
-        EXISTS (SELECT 1 FROM akas a2 
-                WHERE a2.titleId = tb.tconst AND a2.title = p_title)
-      )
-  )
-  ORDER BY a.titleId, a.ordering;
-
-  /* ===================== 3) Crew (directores / escritores) ===================== */
-  SELECT
-    tb.tconst,
-    (SELECT GROUP_CONCAT(DISTINCT nb.primaryName ORDER BY nb.primaryName SEPARATOR ', ')
-      FROM crew_directors cd
-      JOIN name_basics nb ON nb.nconst = cd.nconst
-      WHERE cd.tconst = tb.tconst) AS directors,
-    (SELECT GROUP_CONCAT(DISTINCT nb.primaryName ORDER BY nb.primaryName SEPARATOR ', ')
-      FROM crew_writers cw
-      JOIN name_basics nb ON nb.nconst = cw.nconst
-      WHERE cw.tconst = tb.tconst) AS writers
-  FROM title_basics tb
-  WHERE tb.primaryTitle  = p_title
-    OR tb.originalTitle = p_title
-    OR EXISTS (SELECT 1 FROM akas a 
-                WHERE a.titleId = tb.tconst AND a.title = p_title)
-  ORDER BY tb.tconst;
-
-  /* ===================== 4) Principals (reparto/equipo) ===================== */
-  SELECT
-    p.tconst,
-    p.ordering,
-    p.category,
-    p.job,
-    p.characters,
-    p.nconst,
-    nb.primaryName,
-    (SELECT GROUP_CONCAT(np.profession ORDER BY np.profession SEPARATOR ', ')
-      FROM name_professions np
-      WHERE np.nconst = p.nconst) AS professions
-  FROM principals p
-  JOIN name_basics nb ON nb.nconst = p.nconst
-  WHERE EXISTS (
-    SELECT 1 FROM title_basics tb
-    WHERE tb.tconst = p.tconst
-      AND (
-        tb.primaryTitle  = p_title OR
-        tb.originalTitle = p_title OR
-        EXISTS (SELECT 1 FROM akas a 
-                WHERE a.titleId = tb.tconst AND a.title = p_title)
-      )
-  )
-  ORDER BY p.tconst, p.ordering;
-
-  /* ===================== 5) Episodios (si es serie) ===================== */
-  SELECT
-    e.parentTconst        AS series_tconst,
-    s.primaryTitle        AS seriesTitle,
-    e.seasonNumber,
-    e.episodeNumber,
-    e.tconst              AS episode_tconst,
-    ep.primaryTitle       AS episodeTitle,
-    r.averageRating       AS episodeRating,
-    r.numVotes            AS episodeVotes
-  FROM episodes e
-  JOIN title_basics s  ON s.tconst  = e.parentTconst
-  JOIN title_basics ep ON ep.tconst = e.tconst
-  LEFT JOIN ratings r  ON r.tconst  = e.tconst
-  WHERE EXISTS (
-    SELECT 1 FROM title_basics tb
-    WHERE tb.tconst = e.parentTconst
-      AND (
-        tb.primaryTitle  = p_title OR
-        tb.originalTitle = p_title OR
-        EXISTS (SELECT 1 FROM akas a 
-                WHERE a.titleId = tb.tconst AND a.title = p_title)
-      )
-  )
-  ORDER BY e.parentTconst, e.seasonNumber, e.episodeNumber;
 END;
 
 -- LAS REQUERIDAS EN EL CORREO
